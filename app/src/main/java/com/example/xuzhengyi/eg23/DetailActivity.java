5b050eae50099fca4a0d52fc32df0d578b8195ac
xuzhengyi1995 <zhengyi.xu@utt.fr>
2017-06-09 06:01:52 +0800

    Finished detail.

--- app/src/main/java/com/example/xuzhengyi/eg23/DetailActivity.java
+++ app/src/main/java/com/example/xuzhengyi/eg23/DetailActivity.java
@@ -5,6 +5,16 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
 
+import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.components.Description;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.data.PieEntry;
+import com.github.mikephil.charting.utils.ColorTemplate;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class DetailActivity extends AppCompatActivity {
 
     @Override
@@ -20,10 +30,40 @@ public class DetailActivity extends AppCompatActivity {
                 onBackPressed();
             }
         });
+
+        Draw_pie();
+
     }
 
     @Override
     public void onBackPressed() {
         super.onBackPressed();
+    }
+
+    public void Draw_pie(){
+        PieChart chart=(PieChart) findViewById(R.id.chart);
+        List<PieEntry> entries =new ArrayList<PieEntry>();
+        entries.add(new PieEntry(47l,""));
+        entries.add(new PieEntry(29l,""));
+        entries.add(new PieEntry(21l,""));
+        entries.add(new PieEntry(28l,""));
+        entries.add(new PieEntry(80l,""));
+        entries.add(new PieEntry(30l,""));
+        entries.add(new PieEntry(49l,""));
+        entries.add(new PieEntry(18l,""));
+
+        PieDataSet dataSet=new PieDataSet(entries,"");
+        dataSet.setColors(ColorTemplate.COLORFUL_COLORS);
+        PieData data=new PieData(dataSet);
+        chart.setData(data);
+        Description description= new Description();
+        description.setText("Frappes");
+        chart.setDescription(description);
+
+        chart.setCenterText("340");
+        chart.setDrawCenterText(true);
+
+        chart.animateXY(3000,3000);
+
     }
 }
